71fd2001d5bdd24e42812ed2204a4c49b6bb6365
hd2yao <1054290355@qq.com>
2023-07-25 15:52:36 +0800

    feat(errors):define errors

---
+++ errors.go
@@ -0,0 +1,26 @@
+package openwechat
+
+import "errors"
+
+var (
+    // ErrForbidden define login forbidden error
+    ErrForbidden = errors.New("login forbidden")
+
+    // ErrInvalidStorage define invalid storage error
+    ErrInvalidStorage = errors.New("invalid storage")
+
+    // NetworkErr define wechat network error
+    NetworkErr = errors.New("wechat network error")
+
+    // ErrNoSuchUserFoundError define no such user found error
+    ErrNoSuchUserFoundError = errors.New("no such user found")
+
+    // ErrLoginTimeout define login timeout error
+    ErrLoginTimeout = errors.New("login timeout")
+
+    // ErrWebWxDataTicketNotFound define webwx_data_ticket not found error
+    ErrWebWxDataTicketNotFound = errors.New("webwx_data_ticket not found")
+
+    // ErrUserLogout define user logout error
+    ErrUserLogout = errors.New("user logout")
+)
